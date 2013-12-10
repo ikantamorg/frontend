21e3e7cff7564b81f79085077165802958423b15
iKantam <pshumskiy@ikantam.com>
2013-12-10 17:52:17 +0300

    Update ikantam.js

--- assets/js/ikantam.js
+++ assets/js/ikantam.js
@@ -2,42 +2,25 @@
  *  Written for: ?
  *  Author: @IkantamCorp - @ElmahdiMahmoud
  *  Further changes: @author
- *  Last modified date: 02.12.13
+ *  Updated: 10.12.13
  */
 
  (function($) { 
  $(document).ready(function() { 
 	
-	$.fn.stepTo = function () {
-		return this.each(function () {
-		var $this = $(this);
-		$this.on('click', function(){
-		$('html,body').animate({
-			scrollTop: $(this.hash).offset().top - 85
-		}, 1000);
-		return false;
-		});
-		});
-	}
-	$('.step-to').stepTo();
 	
-	/*!
-	 * Basic plugin pattern
-	 */
-	/*
-	// self-invoking
-	;(function ($, window, document, undefined) {
-	    $.fn.plugiName = function () {
-	        return this.each(function () {
-	            var $this = $(this);
-	            //plugin logic
-	        });
-	    }
-	})(jQuery, window, document);
-	//fire the plugin
-	$('selector').plugiName();
-	*/
-
+	/* CODE GOES HERE */
+	
+	
+	
 	
  }); //end doc ready
 })(jQuery); 
+
+
+/* 
+ * PLUGINS 
+ */
+
+/* stepTo: smooth target scroll */
+$.fn.stepTo=function(){return this.each(function(){var a=$(this);a.on("click",function(){$("html,body").animate({scrollTop:$(this.hash).offset().top-85},1000);return false})})};
