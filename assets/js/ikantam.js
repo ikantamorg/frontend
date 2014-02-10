2b048644d70d3ada033595166aa7a95398616c89
Mahmoud Elmahdi <elmahdi.mahmoud@gmail.com>
2014-02-10 10:25:30 +0300

    placeholder for oldIE

--- assets/js/ikantam.js
+++ assets/js/ikantam.js
@@ -2,14 +2,33 @@
  *  Written for: ?
  *  Author: @IkantamCorp - @ElmahdiMahmoud
  *  Further changes: @author
- *  Updated: 17.01.14
+ *  Updated: 10.02.14
  */
  (function($) { 
  $(document).ready(function() { 
 	
-	// code goes here!
+ 	/* ############[ placeholder ]############ */
+	$('.ie8 [placeholder], .ie9 [placeholder]').focus(function() {
+		var input = $(this);
+		if (input.val() == input.attr('placeholder')) {
+			input.val('');
+			input.removeClass('placeholder');
+		}
+	}).blur(function() {
+		var input = $(this);
+		if (input.val() == '' || input.val() == input.attr('placeholder')) {
+			input.addClass('placeholder');
+			input.val(input.attr('placeholder'));
+		}
+	}).blur().parents('form').submit(function() {
+		$(this).find('[placeholder]').each(function() {
+			var input = $(this);
+			if (input.val() == input.attr('placeholder')) {
+				input.val('');
+			}
+		})
+	});
 
  }); //end doc ready
 })(jQuery); 
 
-/* TODO: place plugins outside the doc ready! */
