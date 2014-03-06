ae6588de63a3b5d2491819d335e69ea398a5c871
iKantam <pshumskiy@ikantam.com>
2014-03-06 10:04:19 +0300

    Update Gruntfile.js
    setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,82 +1,82 @@
-module.exports = function(grunt) {
-	grunt.initConfig({
-	pkg: grunt.file.readJSON('package.json'),
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-	concat: {
-		options: {
-			separator: ';',
-		},
-		dist: {
-			src: ['assets/js/custom/*.js'],
-			dest: 'assets/js/main.js',
-			nonull: true,
-		}
-	},
+        concat: {
+            options: {
+                separator: ';',
+            },
+            dist: {
+                src: ['assets/js/custom/*.js'],
+                dest: 'assets/js/main.js',
+                nonull: true,
+            }
+        },
 
-	compass: {
-		dist: {
-			options: {
-				noLineComments: true,
-				outputStyle: 'compact',
-				sassDir: 'assets/scss',
-				cssDir: 'css'
-			}
-		}
-	},
+        compass: {
+            dist: {
+                options: {
+                    noLineComments: true,
+                    sassDir: 'assets/scss',
+                    cssDir: 'assets/css'
+                }
+            }
+        },
 
-	imagemin: {                  
-		dynamic: {               
-			files: [{
-				expand: true,        
-				cwd: 'assets/images/modules',  
-				src: ['assets/images/modules/**/*.{png,jpg,gif}'], 
-				dest: 'assets/images'     
-			}]
-		}
-		/*
-		,single: {
-		cwd: 'images/modules',
-		files: 'images/modules/specific.png': 'images/modules/specific2.png', 
-		dest: 'images/modules'
-		}
-		*/
-	},
+        imagemin: {
+            options: {
+                cache: false
+            },
 
-	uglify: {
-		options: {
-			mangle: false
-		},
-		my_target: {
-			files: {
-				'assets/js/main.min.js': ['assets/js/main.js']
-			}
-		}
-	},
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'assets/images/modules/',
+                        src: ['**/*.{png,jpg,gif}'],
+                        dest: 'assets/images/opt/'
+                    }
+                ]
+            }
+        },
 
-	/* ======[ configuration ]====== */
-	watch: {
-		css: {
-			files: '**/*.scss',
-			tasks: ['compass']
-		},
-		scripts: {
-			files: '**/*.js',
-			tasks: ['concat']
-		},
-		images: {
-			files: ['assets/images/modules/**/*.{png,jpg,gif}'],
-			tasks: ['imagemin:dynamic'],
-			options: {
-				spawn: false
-			}
-		},
-		
-		minjs: {
-			files: 'assets/js/main.js',
-			tasks: ['uglify']
-		}
-	}
-	});
+        uglify: {
+            options: {
+                mangle: false
+            },
+            my_target: {
+                files: {
+                    'assets/js/main.min.js': ['assets/js/main.js']
+                }
+            }
+        },
+
+        /* ======[ configuration ]====== */
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['compass']
+            }
+	
+        //,scripts: {
+        //    files: '***/*//*.js',
+        //    tasks: ['concat']
+        //}
+        //,images: {
+        //    files: ['**/*'],
+        //    tasks: ['imagemin'],
+        //    options: {
+        //        spawn: false
+        //    }
+        //}
+
+        //,minjs: {
+        //    files: 'assets/js/main.js',
+        //    tasks: ['uglify']
+       // }
+    }
+}
+);
 
 	/* ======[ dependent plugins ]====== */
 	grunt.loadNpmTasks('grunt-contrib-watch');
@@ -84,6 +84,10 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-compass');
-
+	
 	grunt.registerTask('default',['watch']);
+	//grunt.registerTask('default', ['imagemin']);
+	//grunt.registerTask('default', ['uglify']);
+	//grunt.registerTask('default', ['concat']);
 }
+
